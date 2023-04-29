dbe4f64157c32bd27c8d7c7adcaf3142681c4b5b
ibrahimibu22 <ibrahim2242k@gmail.com>
2023-04-29 15:54:25 +0530

    used_car_price_prediction

---
+++ app.py
@@ -0,0 +1,37 @@
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+# Load the model
+rfrmodel = pickle.load(open('rfrmodel.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=(np.array(list(data.values())).reshape(1,-1))
+    output=rfrmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=rfrmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The Car price prediction is {}".format(output))
+
+
+
+if __name__ == "__main__" :
+    app.run(debug=True)
